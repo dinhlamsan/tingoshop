3164ac9db96ebb976df45e6884a2f4a18bbf7e40
SLDHome <dinhlamsan@live.com>
2016-07-20 20:07:27 +0700

    Create ProductId

--- TiNgoShop.Model/Models/OrderDetail.cs
+++ TiNgoShop.Model/Models/OrderDetail.cs
@@ -5,6 +5,7 @@ namespace TiNgoShop.Model.Models
     [Table("OrderDetails")]
     public class OrderDetail
     {
-        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
     }
 }
